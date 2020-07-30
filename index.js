59de0e54b88b335ebd1c9f95f6e1d303110f8ba2
Andre Ferreira <delcferreira@gmail.com>
2020-07-29 22:37:08 -0300

    v1.2.1 two palyers

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const screen = document.getElementById('screen');
 const context = screen.getContext('2d');
-const currentPlayerId = 'payer2';
 
-let points = 0;
+const currentPlayerId1 = 'payer1';
+const currentPlayerId2 = 'payer2';
+
+let points1 = 0;
+let points2 = 0;
 
 const game = {
     players: {
-        'payer2': { x: 9, y: 9}
+        'payer1': { x: 1, y: 2 },
+        'payer2': { x: 9, y: 9 }
     },
     fruit: {
         'fruit': { x: 1, y: 1 }
@@ -17,20 +21,35 @@ document.addEventListener('keydown', handleKeydown);
 
 function handleKeydown(event) {
     const keyPressed = event.key;
-    const player = game.players[currentPlayerId];
+    const player1 = game.players[currentPlayerId1];
+    const player2 = game.players[currentPlayerId2];
     
-    if (keyPressed === 'ArrowUp' && player.y - 1 >= 0) {
-        player.y = player.y - 1;
+    if (keyPressed === 'ArrowUp' && player2.y - 1 >= 0) {
+        player2.y = player2.y - 1;
     }
-    if (keyPressed === 'ArrowDown' && player.y + 1 < screen.height ) {
-        player.y = player.y + 1;
+    if (keyPressed === 'ArrowDown' && player2.y + 1 < screen.height ) {
+        player2.y = player2.y + 1;
     }
-    if (keyPressed === 'ArrowLeft' && player.x - 1 >= 0) {
-        player.x = player.x - 1;
+    if (keyPressed === 'ArrowLeft' && player2.x - 1 >= 0) {
+        player2.x = player2.x - 1;
     }
-    if (keyPressed === 'ArrowRight' && player.x + 1 < screen.width) {
-        player.x = player.x + 1;
+    if (keyPressed === 'ArrowRight' && player2.x + 1 < screen.width) {
+        player2.x = player2.x + 1;
     }
+
+    if (keyPressed === 'w' && player1.y - 1 >= 0) {
+        player1.y = player1.y - 1;
+    }
+    if (keyPressed === 's' && player1.y + 1 < screen.height ) {
+        player1.y = player1.y + 1;
+    }
+    if (keyPressed === 'a' && player1.x - 1 >= 0) {
+        player1.x = player1.x - 1;
+    }
+    if (keyPressed === 'd' && player1.x + 1 < screen.width) {
+        player1.x = player1.x + 1;
+    }
+
 }
 
 renderScreen()
@@ -40,8 +59,16 @@ function renderScreen() {
 
     for (let payerId in game.players) {
         const player = game.players[payerId];
-        context.fillStyle = 'black';
-        context.fillRect(player.x, player.y, 1, 1);
+
+        if (game.players.payer1 == player) {
+            context.fillStyle = 'black';
+            context.fillRect(player.x, player.y, 1, 1);
+        }
+
+        if (game.players.payer2 == player) {
+            context.fillStyle = 'orange';
+            context.fillRect(player.x, player.y, 1, 1);
+        }
     }
 
     for (let fruitId in game.fruit) {
@@ -56,20 +83,37 @@ function renderScreen() {
 
 function check() {
     const positionFrut = game.fruit.fruit;
-    const positionPlayer = game.players.payer2;
+    const positionPlayer1 = game.players.payer1;
+    const positionPlayer2 = game.players.payer2;
 
-    if (positionFrut.x == positionPlayer.x) {
-        if (positionFrut.y == positionPlayer.y) {
+    if (positionFrut.x == positionPlayer2.x) {
+        if (positionFrut.y == positionPlayer2.y) {
+            pointsRender(2);
+            game.fruit.fruit = { x: Math.floor(Math.random() * 10), y: Math.floor(Math.random() * 10) };
+        }
+    }
 
-            pointsRender();
+    if (positionFrut.x == positionPlayer1.x) {
+        if (positionFrut.y == positionPlayer1.y) {
+            pointsRender(1);
             game.fruit.fruit = { x: Math.floor(Math.random() * 10), y: Math.floor(Math.random() * 10) };
         }
     }
 }
 
-function pointsRender() {
-    const $poit = document.querySelector('div.poit');
+function pointsRender(is) {
 
-    points = points + 1;
-    $poit.innerHTML = `<span>${points}</span>`
+    if (is == 1) {
+        const $poit = document.querySelector('div.poit div.p1');
+
+        points1 = points1 + 1;
+        $poit.innerHTML = `<span>${points1}</span>`
+    }
+
+    else if (is == 2) {
+        const $poit = document.querySelector('div.poit div.p2');
+
+        points2 = points2 + 1;
+        $poit.innerHTML = `<span>${points2}</span>`
+    }
 }
